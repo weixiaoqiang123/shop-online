51b1cab35906b7aea38df3b7137475886b5e6d80
weixiaoqiang <1494129151@qq.com>
2021-04-16 09:31:23 +0800

    编写user模块接口

---
+++ shop-user/src/main/java/com/wxq/user/controller/UserController.java
@@ -0,0 +1,71 @@
+package com.wxq.user.controller;
+
+import com.wxq.bean.User;
+import com.wxq.shopinterface.service.IUserService;
+import com.wxq.util.common.Page;
+import com.wxq.util.common.ResponseVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+  @Autowired
+  private IUserService userService;
+
+  @PostMapping("/user")
+  public ResponseVo<User> login(@RequestBody User user){
+    return userService.login(user);
+  }
+
+  @PostMapping
+  public ResponseVo<User> register(@RequestBody User user){
+    return userService.register(user);
+  }
+
+  @PutMapping
+  public Object update(@RequestBody User user){
+    if(userService.update(user)){
+      return new ResponseEntity<>(HttpStatus.OK);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+  @GetMapping("/{id}")
+  public Object logout(@PathVariable("id") String account, @RequestParam("role") Integer role) {
+    userService.logout(account, role);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{account}")
+  public Object delete(@PathVariable String account,@RequestParam("role") Integer role){
+    if(userService.delete(account, role)){
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+  @GetMapping("/findById")
+  public Object findById(@RequestParam("account") String account, @RequestParam("role") Integer role){
+    User user = userService.findById(account, role);
+    return new ResponseEntity<>(user, HttpStatus.OK);
+  }
+
+  @GetMapping("/findByPage")
+  public Page<User> findByPage(@RequestParam(required = false) Map<String, String> user,
+                               @RequestParam("currentPage") Integer currentPage,
+                               @RequestParam("lineSize") Integer lineSize){
+    Page<User> page = userService.findByPage(user, currentPage, lineSize);
+    return page;
+  }
+
+  @GetMapping("/checkLogin")
+  public boolean checkLogin(@RequestParam("account") String account, @RequestParam("role") Integer role){
+    return userService.checkLogin(account, role);
+  }
+}
---
+++ shop-user/src/main/java/com/wxq/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,155 @@
+package com.wxq.user.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.wxq.bean.User;
+import com.wxq.shopinterface.mapper.UserMapper;
+import com.wxq.shopinterface.service.IUserService;
+import com.wxq.util.common.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+  @Autowired
+  private UserMapper userMapper;
+
+  @Autowired
+  private RedisUtil redisUtil;
+
+  @Override
+  public ResponseVo<User> login(User user) {
+    ResponseVo<User> responseVo = new ResponseVo<>(false, ErrorCode.FAIL, "登录失败");
+    QueryWrapper<User> wrapper = new QueryWrapper<>();
+    wrapper.eq("account", user.getAccount());
+    wrapper.eq("role", user.getRole());
+    User userByDb = userMapper.selectOne(wrapper);
+    if(userByDb == null){
+      responseVo.setMsg("用户不存在");
+    }else {
+      String password = userByDb.getPassword();
+      if(user.getPassword().equals(password)){
+        responseVo.setInstance(true, ErrorCode.SUCCESS, "登录成功", userByDb);
+        // 获取jedis
+        Jedis jedis = redisUtil.getJedis();
+        // 放入redis ,必须起key=user:userId:info
+        String userKey = ConstUtil.getKey(user.getAccount(), user.getRole());
+        // 保存数据
+        jedis.setex(userKey, ConstUtil.KEY_TIME_OUT, JSON.toJSONString(userByDb));
+        // 关闭资源
+        jedis.close();
+      }else {
+        responseVo.setMsg("密码错误");
+      }
+    }
+    return responseVo;
+  }
+
+  /**
+   * 1. 初始化账号余额
+   * 2. 校验昵称是否重复
+   * 3. 校验账号是否注册
+   *    a. 未注册 新增
+   *    b. 注册 判断is_delete是否为0
+   *      (1) 为0 该账号已经注册
+   *      (2) 不为0 账号已经注销，修改用户信息并将is_delete修改为0
+   * @param user 用户信息
+   * @return
+   */
+  @Override
+  public ResponseVo<User> register(User user) {
+    ResponseVo<User> responseVo = new ResponseVo<>(false, ErrorCode.FAIL, "注册失败");
+    user.setBalance(new BigDecimal(0));
+    String account = user.getAccount();
+    String nickname = user.getNickname();
+    Integer role = user.getRole();
+    QueryWrapper<User> wrapper = new QueryWrapper<>();
+    wrapper.eq("nickname", nickname);
+    wrapper.eq("role", role);
+    boolean flag = false;
+    if(userMapper.selectOne(wrapper) != null){
+      responseVo.setMsg("昵称重复");
+    }else {
+      QueryWrapper<User> wrapper1 = new QueryWrapper<>();
+      wrapper1.eq("account", account);
+      wrapper1.eq("role", role);
+      User userByDb = userMapper.selectOne(wrapper1);
+      if(userByDb == null){
+        flag = userMapper.insert(user) == 1;
+      }else {
+        Integer isDelete = userByDb.getIsDelete();
+        if(isDelete == 0){
+          responseVo.setMsg("账号已被注册");
+        }else {
+          user.setIsDelete(0);
+          UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+          updateWrapper.eq("account", account);
+          updateWrapper.eq("is_delete", isDelete);
+          updateWrapper.eq("role", role);
+          flag = userMapper.update(user, updateWrapper) == 1;
+        }
+      }
+    }
+
+    if(flag){
+      responseVo.setInstance(true, ErrorCode.SUCCESS, "注册成功", null);
+      return responseVo;
+    }
+    return responseVo;
+  }
+
+  @Override
+  public boolean update(User user) {
+    return userMapper.updateByCondition("t_sys_user",Wrappers.update()
+        .set("nickname", user.getNickname())
+        .set("name", user.getName())
+        .set("head_img", user.getHeadImg())
+        .eq("account", user.getAccount())) == 1;
+  }
+
+  @Override
+  public void logout(String account, Integer role) {
+    Jedis jedis = redisUtil.getJedis();
+    String userKey = ConstUtil.getKey(account, role);
+    jedis.del(userKey);
+    jedis.close();
+  }
+
+  @Override
+  public boolean delete(String account, Integer role) {
+    UpdateWrapper<User> wrapper = new UpdateWrapper<>();
+    wrapper.eq("account", account);
+    wrapper.eq("role", role);
+    return userMapper.updateByCondition("user", Wrappers.update()
+        .set("is_delete", 1)
+        .eq("account", account)
+        .eq("role", role)) == 1;
+  }
+
+  @Override
+  public User findById(String account, Integer role) {
+    QueryWrapper<User> wrapper = new QueryWrapper<>();
+    wrapper.eq("account", account);
+    wrapper.eq("role", role);
+    wrapper.eq("is_delete", 0);
+    return userMapper.selectOne(wrapper);
+  }
+
+  @Override
+  public Page<User> findByPage(Map<String, String> user, Integer currentPage, Integer lineSize) {
+    return PageQuery.query(userMapper, currentPage, lineSize, user);
+  }
+
+  @Override
+  public boolean checkLogin(String account, Integer role) {
+    Jedis jedis = redisUtil.getJedis();
+    String userJson = jedis.get(ConstUtil.getKey(account, role));
+    return userJson == null;
+  }
+}
