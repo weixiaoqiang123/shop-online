fd19602b52b939a0ccb191ab3678498fe91565ae
weixiaoqiang <1494129151@qq.com>
2021-04-20 14:49:28 +0800

    工具类

---
+++ shop-goods/src/main/java/com/wxq/goods/util/PictureFormatUtil.java
@@ -0,0 +1,6 @@
+package com.wxq.goods.util;
+
+public class PictureFormatUtil {
+  public static final String JPG = "JPG";
+  public static final String PNG = "PNG";
+}
--- shop-interface/src/main/java/com/wxq/shopinterface/mapper/CommonMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wxq.shopinterface.mapper;
-
-/**
- * @author weixiaoqiang
- * @date 2021/4/13 16:23
- */
-public interface CommonMapper<T>{
-}
